3a0af6e9b959d47d0e66be8a9e394888c5f527e1
Vitor Oliveira Dos Santos <51249323+vitor-o-s@users.noreply.github.com>
2021-09-29 16:01:03 -0300

    Add files via upload

---
+++ Azure/main.tf
@@ -0,0 +1,80 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageacc2"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
+  name               = "example"
+  storage_account_id = azurerm_storage_account.example.id
+
+  properties = {
+    hello = "aGVsbG8="
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "exampleworkspace"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_log_analytics_linked_storage_account" "example" {
+  data_source_type      = "customlogs"
+  resource_group_name   = azurerm_resource_group.example.name
+  workspace_resource_id = azurerm_log_analytics_workspace.example.id
+  storage_account_ids   = [azurerm_storage_account.example.id]
+}
+
+resource "azurerm_synapse_workspace" "example" {
+  name                                 = "example"
+  resource_group_name                  = azurerm_resource_group.example.name
+  location                             = azurerm_resource_group.example.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.example.id
+  sql_administrator_login              = "yourlogin"
+  sql_administrator_login_password     = "yourpassword"
+
+  aad_admin {
+    login     = "AzureAD Admin"
+    object_id = "00000000-0000-0000-0000-000000000000"
+    tenant_id = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    Env = "production"
+  }
+}
+
+
+# Para executar abra o PowerShell na azure e execute os comandos na ordem
+
+# terraform init
+# terraform plan 
+# terraform apply
+
